c02180b6ab78c35dab5c30a56edb33cd81ae13e3
Jhonsalazar07 <100879551+Jhonsalazar07@users.noreply.github.com>
2024-05-22 17:34:40 -0500

    Actualizacion 22/05/2024_5:33pm
    1. Programacion de la funcion consultar
    2. Validacion de existencia de ID de pacientes para evitar duplicacion.

--- code/app.java
+++ code/app.java
@@ -8,27 +8,61 @@ public class app {
         fnt_menuPrincipal(true);
     }
     private static void fnt_Pacientes(){
-        String opcionesPaciente = JOptionPane.showInputDialog(null, "Menu pacientes\n\n1. Registrar\n2. Consultar\n3. Salir");
-        if (opcionesPaciente.equals("1")){
-            if (opcionesPaciente.equals("1")){
-                String nombre = JOptionPane.showInputDialog(null, "Nombre");
+        String opcionesPac = JOptionPane.showInputDialog(null, "Menu pacientes\n\n1. Registrar\n2. Consultar\n3. Salir");
+            if (opcionesPac.equals("1")){
+                boolean sw = false;
                 String id = JOptionPane.showInputDialog(null, "Id");
-                int edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Edad"));
-                String contacto = JOptionPane.showInputDialog(null, "Contacto");
-                String historia = JOptionPane.showInputDialog(null, "Historia");
-                pacientes.add(new cls_pacientes(nombre, id, edad, contacto, historia));
-                JOptionPane.showMessageDialog(null, "Paciente Registrado con exito");
-                
-        
+                for (int i = 0; i <pacientes.size(); i++){
+                    if (pacientes.get(i).getIdStr().equals(id)){
+                        sw = true;
+                        break;
+                    }   
+                }
+                if (sw == false){
+                    String nombre = JOptionPane.showInputDialog(null, "Nombre");
+                    int edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Edad"));
+                    String contacto = JOptionPane.showInputDialog(null, "Contacto");
+                    String historia = JOptionPane.showInputDialog(null, "Historia");
+                    pacientes.add(new cls_pacientes(nombre, id, edad, contacto, historia));
+                    JOptionPane.showMessageDialog(null, "Paciente Registrado con exito");
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Este paciente ya se encuentra registrado");
+                }
             }
-    
+            if (opcionesPac.equals("2")){
+                boolean sw = false;
+                int pos = 0;
+                String id = JOptionPane.showInputDialog(null, "Id");
+                for (int i = 0; i <pacientes.size(); i++){
+                    if (pacientes.get(i).getIdStr().equals(id)){
+                        sw = true;
+                        pos = i;
+                        break;
+                    }
+                }
+            
+                if (sw == true){
+                    JOptionPane.showMessageDialog(null,
+                    "Nombre: " + pacientes.get(pos).getNombreStr() + 
+                    "\n Edad " + pacientes.get(pos).getEdadInt() + 
+                    "\n Contacto: " + pacientes.get(pos).getContactoStr() +
+                    "\n Historia: " + pacientes.get(pos).getHistoriaStr());
+                }
+                else{
+                
+                    JOptionPane.showMessageDialog(null, "Este paciente no se encuentra registrado");
+                }
+            }
+            
         }
 
-    }
     private static void fnt_menuPrincipal(boolean m){
         while (m == true) {
-            String opcionesStr = JOptionPane.showInputDialog(null, "Menu Principal\n\n1. Nuevo Paciente\n2. Nueva cita\n4. Salir");
-            
+            String opcionesStr = JOptionPane.showInputDialog(null, "Menu Principal\n\n1. Nuevo Paciente\n2. Nueva cita\n3. Reporte\n4. Salir");
+            if (opcionesStr.equals("1")){
+                fnt_Pacientes();
+            }
         }
     }
 }
